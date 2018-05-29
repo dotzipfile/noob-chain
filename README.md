001ab19049e80d604066a328b2635b4a01ea1442
Davy <davenickmalone@gmail.com>
2018-05-30 00:26:41 +0100

    added conversion to json

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # noob-chain
+
+Simple java block chain proof of concept
+
+To compile you'll need to import google's GSON library
--- src/com/dotzipfile/noob_chain/NoobChain.java
+++ src/com/dotzipfile/noob_chain/NoobChain.java
@@ -2,6 +2,8 @@ package com.dotzipfile.noob_chain;
 
 import java.util.ArrayList;
 
+import com.google.gson.GsonBuilder;
+
 import com.dotzipfile.noob_chain.models.Block;
 
 public class NoobChain {
@@ -10,8 +12,11 @@ public class NoobChain {
 
 	public static void main(String[] args) {
 
-		blockChain.add(new Block("Hi I'm the first block.", "0"));
-		blockChain.add(new Block("Hi I'm the second block.", blockChain.get(blockChain.size() - 1).getHash()));
-		blockChain.add(new Block("Hi I'm the third block.", blockChain.get(blockChain.size() - 1).getHash()));
+		blockChain.add(new Block("Hi this is the first block.", "0"));
+		blockChain.add(new Block("Hi this is the second block.", blockChain.get(blockChain.size() - 1).getHash()));
+		blockChain.add(new Block("Hi this is the third block.", blockChain.get(blockChain.size() - 1).getHash()));
+
+		String blockChainJSON = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
+		System.out.println(blockChainJSON);
 	}
 }
