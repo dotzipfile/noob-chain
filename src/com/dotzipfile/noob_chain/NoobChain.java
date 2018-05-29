87793ae5d68b380b2a9bf495ca2fc8a759453e21
Davy <davenickmalone@gmail.com>
2018-05-29 23:51:00 +0100

    added all blocks to an arraylist

--- src/com/dotzipfile/noob_chain/NoobChain.java
+++ src/com/dotzipfile/noob_chain/NoobChain.java
@@ -1,19 +1,17 @@
 package com.dotzipfile.noob_chain;
 
+import java.util.ArrayList;
+
 import com.dotzipfile.noob_chain.models.Block;
 
 public class NoobChain {
 
+	public static ArrayList<Block> blockChain = new ArrayList<Block>(); 
+
 	public static void main(String[] args) {
 
-		Block genesisBlock = new Block("Hi I'm the first block.", "0");
-		System.out.println("Hash for block 1: " + genesisBlock.getHash());
-
-		Block secondBlock = new Block("Hi I'm the second block.", genesisBlock.getHash());
-		System.out.println("Hash for block 2: " + secondBlock.getHash());
-
-		Block thirdBlock = new Block("Hi I'm the third block.", secondBlock.getHash());
-		System.out.println("Hash for block 3: " + thirdBlock.getHash());
+		blockChain.add(new Block("Hi I'm the first block.", "0"));
+		blockChain.add(new Block("Hi I'm the second block.", blockChain.get(blockChain.size() - 1).getHash()));
+		blockChain.add(new Block("Hi I'm the third block.", blockChain.get(blockChain.size() - 1).getHash()));
 	}
-
 }
